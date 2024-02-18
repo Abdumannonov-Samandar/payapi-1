1c8d0b8314bb218d8a80b2450f85395d5c029cb1
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-02-18 18:26:54 +0500

    header build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      blue: "#36536B",
+      "red-light": "#BA4270",
+      dark: "#242A45",
+      oq: "#FBFCFE",
+      transparent: "transparent",
+    },
+    fontFamily: {
+      sans: ["Public Sans", "sans-serif"],
+    },
+    boxShadow: {
+      'bg-sh' : '0px 10px 25px -10px rgba(54, 83, 107, 0.25)',
+      'bg-hid' : '10px 10px 25px -10px rgba(54, 83, 107, 0.25)',
+    },
   },
   plugins: [],
 };
