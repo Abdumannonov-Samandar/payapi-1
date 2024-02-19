7f270169cbe7f8aeb13b6b814adee21e1f7afbb1
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-02-19 10:36:31 +0500

    hero section build

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,12 @@ module.exports = {
     },
     fontFamily: {
       sans: ["Public Sans", "sans-serif"],
+      serif: ["DM Serif Display", "serif"],
     },
     boxShadow: {
       'bg-sh' : '0px 10px 25px -10px rgba(54, 83, 107, 0.25)',
       'bg-hid' : '10px 10px 25px -10px rgba(54, 83, 107, 0.25)',
+      'btn-shadow' : '10px 10px 25px -10px rgba(54, 83, 107, 0.25)',
     },
   },
   plugins: [],
